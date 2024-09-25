40ecf6b5d804698439c735e514194d3faa599a7c
Aris Irvandiansyah <arisirvandiansyah.dev@gmail.com>
2024-09-25 15:15:26 +0700

    feat(api): add login module

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 exports.register = async (req, res) => {
   const { name, email, password, is_manager } = req.body;
   if (!name || !email || !password) {
@@ -25,5 +26,42 @@ exports.register = async (req, res) => {
       .json({ msg: "User created successfully, please login" });
   } catch (error) {
     return res.status(500).json({ msg: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).send("Please fill all fields");
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).send({ msg: "Your credentials is not valid" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).send({ msg: "Your credentials is not valid" });
+    const payload = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      is_manager: user.is_manager,
+    };
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "20s",
+    });
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+    user.tokens = user.tokens.concat({ token: refreshToken });
+    await user.save();
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    return res
+      .status(200)
+      .send({ msg: "Login successfully, please wait", accessToken });
+  } catch (error) {
+    return res.sendStatus(500).send({ msg: error });
   }
 };
