658f062822add55a8a43a090b8d6f581c2105fa6
Aris Irvandiansyah <arisirvandiansyah.dev@gmail.com>
2024-09-25 14:30:17 +0700

    feat(api): setup express server & connection to mongoDB

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
