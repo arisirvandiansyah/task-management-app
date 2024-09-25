c967b8e6171897821d675111556e7b0edcfce6d5
Aris Irvandiansyah <arisirvandiansyah.dev@gmail.com>
2024-09-25 19:20:21 +0700

    feat(api): add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+exports.authenticate = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    req.user = decoded;
+    next();
+  });
+};
