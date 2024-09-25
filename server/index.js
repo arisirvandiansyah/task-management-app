aaf92c02d5a56d1289165f7e31d436b0351dfd38
Aris Irvandiansyah <arisirvandiansyah.dev@gmail.com>
2024-09-25 20:40:54 +0700

    fix(api): fox cors policy

--- server/index.js
+++ server/index.js
@@ -5,7 +5,12 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 connectDB();
