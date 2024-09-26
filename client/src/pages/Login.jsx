8a1bb2c76186b5e9cef5cba90bb7a07f02bb997c
Aris Irvandiansyah <arisirvandiansyah.dev@gmail.com>
2024-09-26 10:22:08 +0700

    feat(client): add login service

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Error, Success } from "../components/ResponseMsg";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(async () => {
+      await axios
+        .post(`${import.meta.env.VITE_SERVER_URL}/auth/login`, form, {
+          withCredentials: true,
+          credentials: "include",
+        })
+        .then((res) => {
+          setLoading(false);
+          setMsg(<Success msg={res.data.msg} />);
+          setTimeout(() => {
+            navigate("/");
+          }, 3000);
+        })
+        .catch((err) => {
+          setLoading(false);
+          setMsg(<Error msg={err.response.data.msg} />);
+        });
+      setTimeout(() => {
+        setMsg("");
+      }, 3000);
+    }, 1000);
+  };
   return (
     <div className="h-screen flex justify-center items-center px-12">
+      {msg && msg}
       <div className="w-64">
         <div className="p-5 rounded bg-white shadow-lg">
           <div className="mb-3">
@@ -10,22 +47,30 @@ const Login = () => {
             <p>Please fill in this form to login.</p>
           </div>
           <div className="mb-3">
-            <form action="">
+            <form onSubmit={handleSubmit}>
               <div className="mb-3">
                 <label htmlFor="email">Email</label>
                 <input
                   type="email"
                   name="email"
+                  onChange={(e) => setForm({ ...form, email: e.target.value })}
                   placeholder="eg: 0qTgG@example.com"
                 />
               </div>
               <div className="mb-3">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" placeholder="******" />
+                <input
+                  type="password"
+                  name="password"
+                  onChange={(e) =>
+                    setForm({ ...form, password: e.target.value })
+                  }
+                  placeholder="******"
+                />
               </div>
               <div className="mb-3">
-                <button type="submit" className="w-full">
-                  Login
+                <button type="submit" className="w-full" disabled={loading}>
+                  {loading ? "Loading..." : "Login"}
                 </button>
               </div>
             </form>
