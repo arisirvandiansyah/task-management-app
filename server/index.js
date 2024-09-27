e4c3fd0f11af3220f8362ba3069f01210654ff31
Aris Irvandiansyah <arisirvandiansyah.dev@gmail.com>
2024-09-27 08:49:29 +0700

    #

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://task-management-app-gray.vercel.app/",
     credentials: true,
   })
 );
